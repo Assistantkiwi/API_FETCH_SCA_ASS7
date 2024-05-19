2dc8b204d85e0d0ec83240b19dc24588bab3d03d
Dabi-ayu <dabiayu8@gmail.com>
2024-05-19 07:01:23 +0100

    added the search functionality

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let apiKey = "Df3czzOiBVrW8s1LnML7HQ49SnZnPKLg";
-const displayDiv = document.getElementById("gif-div");
+const displayDiv = document.getElementById("gif-container");
 
-const displayEmoji = async () => {
+const displayGif = async () => {
+  let userInput = document.getElementById("search-box").value.trim();
+  document.getElementById("search-box").value = "";
+
+  if (userInput === "") {
+    return;
+  }
+  displayDiv.innerHTML = "";
   try {
     const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${apiKey}&limit=25`
+      `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${apiKey}&limit=25`
     );
     const data = await response.json();
     data.data.map((item) => {
